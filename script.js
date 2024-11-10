bcc4b50888330e7eb4a331d99695c8ae66fcfaab
Nazar <159822046+ToNaza@users.noreply.github.com>
2024-11-10 21:03:09 +0200

    Fix bug

--- script.js
+++ script.js
@@ -31,23 +31,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    const footer = document.getElementById("footer");
-    const footerTrigger = document.getElementById("footer-trigger");
-  
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          footer.classList.add("visible"); // Показати футер
-        } else {
-          footer.classList.remove("visible"); // Сховати футер, якщо він більше не видимий
-        }
-      });
-    }, {
-      threshold: 1.0 // Тільки коли весь footer-trigger буде в полі зору
+  const footer = document.getElementById("footer");
+  const footerTrigger = document.getElementById("footer-trigger");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        footer.classList.add("visible"); // Показати футер
+      } else {
+        footer.classList.remove("visible"); // Сховати футер, якщо він більше не видимий
+      }
     });
-  
-    observer.observe(footerTrigger);
+  }, {
+    threshold: 1.0 // Тільки коли весь footer-trigger буде в полі зору
   });
+
+  observer.observe(footerTrigger);
+});
+  
   
   
   document.addEventListener("DOMContentLoaded", function() {
